f5e27b0db0ff867754741b6a26c2779f0c0b3059
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-03-14 20:08:05 -0400

    Add manual sync of existing files

--- sdpp.py
+++ sdpp.py
@@ -79,6 +79,36 @@ def init_paperpile_to_dpt(paperpile_path : str = None,
                        'modtime': entry.stat().st_mtime})
 
 
+def sync_existing(dry_run=True, verbose=True):
+    """Manually sync files by comparing modification time with last known.
+    """
+    home = os.path.expanduser('~/')
+    truncate = len(home)
+    os.chdir(home)
+    db = TinyDB('.sdpp-sync.json')
+    for elem in db.all():
+        sony, paperpile, modtime = [elem[k]
+                                    for k in ['box', 'paperpile', 'modtime']]
+        smod = os.stat(sony).st_mtime
+        pmod = os.stat(paperpile).st_mtime
+        if smod > modtime + 10:  # 10 second resolution
+            if pmod > modtime + 10:
+                print('both %s and %s changed; resolve manually.' %
+                      (sony, paperpile))
+            else:
+                if dry_run or verbose:
+                    print('copying updated %s to %s' % (sony, paperpile))
+                if not dry_run:
+                    elem['modtime'] = smod
+                    shutil.copy2(sony, paperpile)
+        elif pmod > modtime + 10:
+            if dry_run or verbose:
+                print('copying update %s to %s' % (paperpile, sony))
+            if not dry_run:
+                elem['modtime'] = pmod
+                shutil.copy2(paperpile, sony)
+
+
 def main():
     import argparse
     p = argparse.ArgumentParser(description='sync paperpile and box')
@@ -86,11 +116,15 @@ def main():
                    help='Run initial sync')
     p.add_argument('--do', action='store_true',
                    help='Actually sync, not just dry run.')
+    p.add_argument('--syncx', action='store_true',
+                   help='Manually sync existing files.')
 
     args = p.parse_args()
 
     if args.init:
         init_paperpile_to_dpt(dry_run=(not args.do))
+    elif args.syncx:
+        sync_existing(dry_run=(not args.do))
 
 
 if __name__ == '__main__':
